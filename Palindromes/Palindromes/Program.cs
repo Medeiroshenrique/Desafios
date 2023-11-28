a7c77f84c7e917a8f01153ea64e9193bf7eb9385
Henrique  Medeiros <henrique.c.r.medeiros@gmail.com>
2023-11-27 21:17:30 -0300

    :feat: palindromes identificator

--- Palindromes/Palindromes/Program.cs
+++ Palindromes/Palindromes/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Palindromes{
+    public class Program {
+        public static bool IsPalindromeOrNot(string word) {
+            string reverseWord ="";
+            char[] wordChars = word.ToCharArray();
+            for (int i = wordChars.Length -1; i >= 0; i--) {
+                reverseWord += wordChars[i].ToString();
+            }
+
+            if (word == reverseWord)
+            {
+                Console.WriteLine("Palindrome!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Not palindrome!");
+                return false;
+            }
+        }
+        public static void Main() {
+            string RandomWord = "arara";
+            IsPalindromeOrNot(RandomWord);
+        }
+    }
+}
